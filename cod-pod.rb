623916d70008e81bf4913c10b2829d3a13dcb59a
Chris Rimmer <c@24.org.uk>
2014-02-10 18:36:52 +0000

    Changed so it still works when mp3s dir is empty

--- cod-pod.rb
+++ cod-pod.rb
@@ -2,6 +2,9 @@ require 'sinatra'
 
 get '/cod-pod.xml' do
   content_type 'application/rss+xml'
-  @dir = Dir.pwd + "/mp3s"
+  dir = Dir.pwd + "/mp3s"
+  @files = Dir[dir + "/*.mp3"]
+  timestamps = @files.map{|f| File.mtime(f)}
+  @published = timestamps.empty? ? Time.now : timestamps.max
   erb :rss
 end
